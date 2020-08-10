8c23aafae75b2ae5f5056fd899371e0a523e966e
Gabrielle Hoover <grh39@drexel.edu>
2020-08-10 13:30:41 -0400

    Hexcode work, redo function

--- common/scripts/index.js
+++ common/scripts/index.js
@@ -14,29 +14,38 @@ picker_tool.addEventListener('mousedown', function() {
 
 // Function displays selected modal. Accepts paramater of the clicked element's ID.
 function openModal(e) {
-    var modal = document.getElementById(`${e}_modal`);
-    modal.classList.add('active');
+    window.modal = document.getElementById(`${e}_modal`);
+    window.modal.classList.add('active');
 
     closeBtn = document.querySelector('.active .close');
     closeBtn.addEventListener('mousedown', function () {
-        modal.classList.remove('active');
+        window.modal.classList.remove('active');
     });
 
-    // If user clicks outside of the modal content box, modal will clos
+    // If user clicks outside of the modal content box, modal will close
     // Note this requires content inside of the modal content box to work (not positioned absolutely)
     window.onclick = function(event) {
         if (event.target == modal) {
-            modal.classList.remove('active');
+            window.modal.classList.remove('active');
         }
     }
 };
+
+//container for previous actions for undo
+const theStack = [];
+const redoStack = []; //another one
 
 //initialize undo button
 const undoBtn = document.getElementById('undoButton');
 undoBtn.addEventListener('click', undoColor);
 
-//container for previous actions for undo
-const theStack = [];
+//initialize redo button
+const redoBtn = document.getElementById('redoButton');
+redoBtn.addEventListener('click', redoColor);
+
+//confirmation button for color picker
+const confirm = document.getElementById('confirm');
+confirm.addEventListener('click', cachecolor);
 
 //convert rgb to hex
 function RGBToHex(rgb) {
@@ -60,25 +69,41 @@ function RGBToHex(rgb) {
   }
 
 //display hex code when picker is used
-//also cache it as an option
 var colorswatches = document.getElementsByClassName("swatch");
-let customcolori = 0;
 colorPicker.on('color:change', function(color) {
-    window.newColor = document.getElementById('hexchange').innerHTML=(color.hexString);
-    //use the almighty modulo to cache the new color
-    colorswatches[customcolori%5].style.background=color.hexString;
-    customcolori+= 1;
+    window.newColor = document.getElementById('hexchange').innerHTML = (color.hexString);
+    document.getElementById('selected-color-shower').style.backgroundColor=window.newColor;
 });
 
+//cache color as an option
+let customcolori = 0;
+function cachecolor() {
+    //use the almighty modulo to cache the new color
+    colorswatches[customcolori%5].style.background=window.newColor;
+    customcolori+= 1;
+    //close the modal upon adding new color
+    window.modal.classList.remove('active');
+}
 
 //recolor SVG
 //also add previous item/color combo to stack for undo function
 function clickEvent(evt) {
+<<<<<<< Updated upstream
     window.prevItem = evt.target;
     window.prevColor=(evt.target.getAttribute('fill'));
     theStack.push([window.prevItem, window.prevColor]);
     //below is what changes the color
     evt.target.setAttribute('fill', currentColor);
+=======
+    const target = evt.target;
+    if (target.classList.contains('paintable')) {
+      window.prevItem = evt.target;
+      window.prevColor=(evt.target.getAttribute('fill'));
+      theStack.push([window.prevItem, window.prevColor]);
+      //below is what changes the color
+      evt.target.setAttribute('fill', currentColor);
+    }
+>>>>>>> Stashed changes
 }
 
 //set new color to pencil in
@@ -100,7 +125,21 @@ function undoColor(){
             theColor = checkDefault(theObject);
         }
         theObject.setAttribute('fill', theColor);
-        theStack.pop();
+        redoStack.push(theStack.pop()); //pop from undo to redo
+        console.log(redoStack);
+    }
+}
+
+function redoColor(){
+    console.log('hi')
+    if (redoStack.length>0){
+        let theObject = redoStack[redoStack.length-1][0];
+        let theColor = redoStack[redoStack.length-1][1];
+        if(theColor == null){
+            theColor = checkDefault(theObject);
+        }
+        theObject.setAttribute('fill', theColor);
+        theStack.push(redoStack.pop());
     }
 }
 
