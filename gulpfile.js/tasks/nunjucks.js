04d26e234a089e98f543b6fd2b00c19f4282e219
Erik-Martus <ekm66@drexel.edu>
2020-08-16 20:02:40 -0400

    :construction: Update pages to use nunjucks
    Index working as expected, image pages not currently working, need further assistance

--- gulpfile.js/tasks/nunjucks.js
+++ gulpfile.js/tasks/nunjucks.js
@@ -1,17 +1,28 @@
-const path = require('path')
+const path = require("path");
 // const { src, dest, watch } = require('gulp')
-const { src, dest } = require('gulp')
-const nunjucksRender = require('gulp-nunjucks-render')
+const { src, dest } = require("gulp");
+const nunjucksRender = require("gulp-nunjucks-render");
+const data = require("gulp-data");
 
 function nunjucksTask() {
-    // get .html|.njk files in 'pages'
-    return src(path.join(__dirname, '../../src/pages/**/*.+(html|njk)'))
-    // render template with nunjucks
-    .pipe(nunjucksRender({
-      path: [path.join(__dirname, '../../src/templates')]
-    }))
-    // output files to build folder
-    .pipe(dest(path.join(__dirname, '../../build')))
-};
+  // get .html|.njk files in 'pages'
+  return (
+    src(path.join(__dirname, "../../src/pages/**/*.+(html|njk)"))
+      // add data from JSON
+      .pipe(
+        data(() => {
+          return require(path.join(__dirname, "../../src/data/data.json"));
+        })
+      )
+      // render template with nunjucks
+      .pipe(
+        nunjucksRender({
+          path: [path.join(__dirname, "../../src/templates")],
+        })
+      )
+      // output files to build folder
+      .pipe(dest(path.join(__dirname, "../../build")))
+  );
+}
 
-module.exports = nunjucksTask
+module.exports = nunjucksTask;
