728507dcb2c1b0aa87e62548ffa97c81437a1d6a
Erik-Martus <ekm66@drexel.edu>
2020-09-04 00:38:02 -0400

    :sparkles: Add html minify

--- gulpfile.js/tasks/eleventy.js
+++ gulpfile.js/tasks/eleventy.js
@@ -1,13 +1,17 @@
 const { src, dest, series, parallel, watch } = require('gulp');
 const cp = require("child_process");
+const htmlmin = require('gulp-htmlmin');
+const gulpif = require('gulp-if');
+const argv = require('yargs').argv
 
 const eleventy = () => {
     return cp.spawn("npx", ["eleventy", "--quiet"], { stdio: "inherit" });
 };
 
-const copy = () => {
+const htmlMinify = () => {
     return src('./temp/**/*')
+        .pipe(gulpif(argv.prod, htmlmin({ collapseWhitespace: true, removeComments: true, html5: true })))
         .pipe(dest("./build"));
 }
 
-module.exports = series(eleventy, copy);
+module.exports = series(eleventy, htmlMinify);
